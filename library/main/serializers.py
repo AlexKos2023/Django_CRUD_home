a03ed02d209fd8501dd1ea6ce30a2832372ff68d
Kostylev Alexander <kos_kpi@mail.ru>
2024-02-27 20:18:44 +0300

    Initial

---
+++ library/main/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from main.models import Book, Order
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    # Дополнительное задание
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['orders_count'] = instance.order_set.count()
+        return representation
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+    # Дополнительное задание
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if isinstance(instance, Order):
+            representation['books'] = BookSerializer(instance.books.all(), many=True).data
+        return representation
